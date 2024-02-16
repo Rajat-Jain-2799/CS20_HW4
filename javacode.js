91262db5daeee9b3998c424d593b046a88c1292c
Rajat-Jain-2799 <157147517+Rajat-Jain-2799@users.noreply.github.com>
2024-02-15 19:45:02 -0500

    Update javacode.js

--- javacode.js
+++ javacode.js
@@ -1,10 +1,19 @@
-//these are all the constant values
-document.addEventListener('DOMContentLoaded', function() {
+/*
+ *  Assignment: Joe’s Hotdogs
+ *  Rajat Jain
+ *  2-12-2024
+ *
+ *  CS 20 Assignment 4
+ *
+ *  Make a functional java scipt for an interactive page for a hotdog stand
+ *
+ */
+    document.addEventListener('DOMContentLoaded', function() {
     const PRICE_HOTDOG = 4.65;
     const PRICE_FRIES = 3.75;
     const PRICE_SODA = 1.89;
     const TAX_RATE = 0.0625; 
-
+    //This is the showmoney function which takes care of the decimal place 
     function showMoney(value) {
         let rounded = Math.round(value * 100) / 100;
         let valueStr = rounded.toString();
@@ -18,14 +27,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         return valueStr;
     }
+    //These are prompts and inputs for asking what all the customers want
     let numDogs = parseInt(prompt("How many hotdogs do you want?"), 10) || 0;
     let numFries = parseInt(prompt("How many fries do you want?"), 10) || 0;
     let numSoda = parseInt(prompt("How many sodas do you want?"), 10) || 0;
     let subtotal = (numDogs * PRICE_HOTDOG) + (numFries * PRICE_FRIES) + (numSoda * PRICE_SODA);
+    //Applys a discout if it meets the discount
     let discount = (subtotal >= 25) ? subtotal * 0.1 : 0;
     subtotal -= discount;
     let tax = subtotal * TAX_RATE;
     let total = subtotal + tax;
+    //This prints out all the necessary outputs for the reciepts
     document.getElementById('orderDetails').innerHTML = `
         <p>Hotdogs Ordered: ${numDogs} ($${showMoney(numDogs * PRICE_HOTDOG)})</p>
         <p>Fries Ordered: ${numFries} ($${showMoney(numFries * PRICE_FRIES)})</p>
